87ac9027319aadb5fc5abef777a3260d0035969a
ayushman577 <ayushmannanda577@gmail.com>
2026-09-06 03:57:57 +0530

    Fix production API URL again and again

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const cors = require('cors');
+
 const cookieParser = require('cookie-parser')
 
 const authRouter = require('./routes/auth.route');
+
 const communityRouter = require('./routes/community.route');
 
 const notificationRoutes =
@@ -15,15 +18,17 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'FRONTEND_URL', // Replace with your frontend URL
-    credentials: true, // Allow credentials (cookies) to be sent
+    origin: 'https://mycomm-chi.vercel.app',
+    credentials: true,
 }));
 
 
 app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
+
 app.use('/api/community', communityRouter);
+
 app.use('/api/notifications', notificationRoutes);
 
 module.exports = app;
