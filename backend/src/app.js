6081c12f95e5f200b01c54b11f148a831957f74a
ayushman577 <ayushmannanda577@gmail.com>
2026-09-06 03:05:30 +0530

    second commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: 'FRONTEND_URL', // Replace with your frontend URL
     credentials: true, // Allow credentials (cookies) to be sent
 }));
 
